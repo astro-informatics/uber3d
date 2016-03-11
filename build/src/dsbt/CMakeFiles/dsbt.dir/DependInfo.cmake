d7d77f6ced53f8d2d8f20f0c7e96b254984cb000
Francois <francois@orioncloud.fr>
2016-03-11 15:18:24 -0800

    Added  DSBT library for radial transform

---
+++ build/src/dsbt/CMakeFiles/dsbt.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/francois/repo/uber3d/src/dsbt/fastDSBT.cpp" "/home/francois/repo/uber3d/build/src/dsbt/CMakeFiles/dsbt.dir/fastDSBT.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/dsbt/CMakeFiles/dsbt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsbt.dir/fastDSBT.cpp.o"
+  "libdsbt.pdb"
+  "libdsbt.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dsbt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
