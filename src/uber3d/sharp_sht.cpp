21f265f3eb6c690ad007f4f92eb10ba1f2b40e1b
Francois <francois@orioncloud.fr>
2016-07-22 13:04:23 -0400

    implememted sharp_sht BUT there is a problem with the sharp library

---
+++ src/uber3d/sharp_sht.cpp
@@ -0,0 +1,32 @@
+#include "sharp_sht.h"
+#include <sharp_cxx.h>
+
+using namespace uber3d;
+
+
+sharp_sht::sharp_sht()
+{
+    sharpjob = new sharp_cxxjob<double>;
+}
+
+sharp_sht::~sharp_sht()
+{
+    delete sharpjob;
+}
+
+
+void sharp_sht::set_healpix_geometry(long int nside)
+{
+    sharpjob.set_Healpix_geometry(nside);
+    
+}
+
+void sharp_sht::forward(const arr< double > in_map, arr< dcomplex > out_alm)
+{
+    sharpjob->map2alm(in_map.begin(), out_alm.begin(), false);
+}
+
+void sharp_sht::backward(const arr< dcomplex > in_alm, arr< double > out_map)
+{
+    sharpjob->alm2map(in_alm.begin(), out_map.begin(), false);
+}
---
+++ src/uber3d/sharp_sht.h
@@ -0,0 +1,62 @@
+#ifndef SHARP_SHT_H
+#define SHARP_SHT_H
+
+#include "sht.h"
+
+// Forward declaration to avoid having to include libsharp
+// headers 
+template<typename T> class sharp_cxxjob;
+
+namespace uber3d {
+    
+    
+/**
+ * Class sharp_sht providing an implementaion of an sht 
+ * based on the libsharp library
+ */
+
+class sharp_sht : public sht
+{
+    
+public:
+    
+   // Constructor
+   //
+   sharp_sht();
+    
+    // Destructor
+    //
+    ~sharp_sht();
+    
+   /**
+    * Initializes libsharp to process healpix geometries
+    * 
+    */
+   void set_healpix_geometry(long nside);
+    
+   /**
+   * Forward SHT transform
+   * \param  in_map Input shell
+   * \param  out_alm output spherical harmonic coefficients
+   */
+   void forward (arr<double> &in_map, arr<dcomplex> &out_alm);
+
+
+  /**
+   * Backward spherical harmonics coefficients
+   * \param  in_alm Input spherical harmonics coefficients
+   * \param  out_map Ouput spherical shell
+   */
+   void backward (arr<dcomplex> &in_alm, arr<double> &out_map);
+    
+private:
+    
+    // Private members
+    sharp_cxxjob<double> *sharpjob;
+    
+};
+
+} // end of package namespace
+
+#endif
+
